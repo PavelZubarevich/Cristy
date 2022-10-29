9df3ab00f3e51bb79cb047d4ea1c8b9d60d88c95
Pavel <pavelzubarevich@gmail.com>
2022-10-29 20:55:35 +0300

    feat: add homeCollection component

---
+++ src/components/HomeCollection/index.js
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import ProductsListing from '../ProductsListing';
+import Button from '../common/Button';
+import './styles.scss';
+
+function HomeCollection() {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const handleViewProducts = () => {
+    navigate('/products');
+  };
+
+  return (
+    <div className="homeCollection container">
+      <div className="homeCollection__collection">
+        <h2 className="homeCollection__collectionLabel">{t('OurCollectionsLabel')}</h2>
+        <p className="homeCollection__collectionText">{t('ColectionDescriptionText')}</p>
+        <div className="homeCollection__listing">
+          <ProductsListing />
+        </div>
+        <Button variant="outlined" onClick={handleViewProducts}>{t('ViewAllProductsLabel')}</Button>
+      </div>
+    </div>
+  );
+}
+
+export default HomeCollection;
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from './Header';
 import Footer from './Footer';
+import HomeCollection from './HomeCollection';
 
 export {
+  HomeCollection,
   Footer,
   Header,
 };
