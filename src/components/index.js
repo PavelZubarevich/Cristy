40612c47c8c89bef7bd12c17b987b40efd0e24dc
Pavel <pavelzubarevich@gmail.com>
2022-10-30 19:36:37 +0300

    feat: add productBanner component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,12 @@
 import Header from './Header';
 import Footer from './Footer';
 import HomeCollection from './HomeCollection';
+import BannerCarousel from './BannerCarousel';
+import ProductBanner from './ProductBanner';
 
 export {
+  ProductBanner,
+  BannerCarousel,
   HomeCollection,
   Footer,
   Header,
