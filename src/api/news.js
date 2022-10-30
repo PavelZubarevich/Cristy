c26f4780b7743a29ed3cb41d086edee4b65f1eaa
Pavel <pavelzubarevich@gmail.com>
2022-10-30 21:53:38 +0300

    add: news api

---
+++ src/api/news.js
@@ -0,0 +1,26 @@
+const news = [
+  {
+    id: 1,
+    title: 'Get 20% OFF on Nike Air Uomo',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
+    date: '2022-10-30T16:31:00Z',
+    image: 'https://as2.ftcdn.net/v2/jpg/04/19/14/17/1000_F_419141782_2mv5HVpvCW4IbixYdJOasaUZfQVfaMAS.jpg',
+  },
+  {
+    id: 2,
+    title: 'Get 20% OFF on Nike Air Uomo',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
+    date: '2022-09-30T16:31:00Z',
+    image: 'https://as2.ftcdn.net/v2/jpg/04/19/14/17/1000_F_419141782_2mv5HVpvCW4IbixYdJOasaUZfQVfaMAS.jpg',
+  },
+  {
+    id: 3,
+    title: 'Get 20% OFF on Nike Air Uomo',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
+    date: '2022-08-30T16:31:00Z',
+    image: 'https://as2.ftcdn.net/v2/jpg/04/19/14/17/1000_F_419141782_2mv5HVpvCW4IbixYdJOasaUZfQVfaMAS.jpg',
+  },
+];
+
+// eslint-disable-next-line
+export const getLastNews = () => news;
